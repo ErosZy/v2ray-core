05d968d5fc14c05ffa26a3017c1e339efeb3086c
zhaoyang@qmtv.com <zy4inlovezx>
2018-11-20 20:58:53 +0800

    [UPD] ci

--- export/raw.go
+++ export/raw.go
@@ -13,3 +13,7 @@ func SetUDPBufferExport(e UDPBufferExport) {
 func GetUDPBufferExport() UDPBufferExport {
 	return exportor
 }
+
+// rm -rf ~/go/src/github.com/ErosZy/AndroidLibV2ray
+// go get github.com/ErosZy/AndroidLibV2ray
+//rm -rf ~/go/src/v2ray.com && rm -rf ~/go/src/ErosZy/v2ray-core &&  make all
